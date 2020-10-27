a8408018d14693c912ac2603564e7981b9da26ff
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 15:31:49 +0530

    initial commit

---
+++ Export_ARM_VMs.ps1
@@ -0,0 +1,12 @@
+﻿Add-AzureRmAccount
+$Subs = Get-AzureRmSubscription | Select-Object Id, Name, TenantId | Out-GridView -Title "Select Subscriptions (use Ctrl/Shift for multiples)" -PassThru
+$VMs = $null
+foreach ($sub in $Subs) {
+    Select-AzureRmSubscription -Subscription $sub.Id
+    $SubId = $sub.Id
+    $SubName = $sub.Name
+    $VMs += Get-AzureRmVM -Status | Select-Object @{n="SubscriptionGuid";e={$SubId}},@{n="SubscriptionName";e={$SubName}},Name, ResourceGroupName, PowerState, Location, {$_.HardwareProfile.VmSize}, {$_.AvailabilitySetReference.Id}
+    
+}
+$VMs | Export-Csv -Path "$($Env:Temp)\ARMVMs.csv" -NoTypeInformation
+Invoke-Item "$($Env:Temp)\ARMVMs.csv"
